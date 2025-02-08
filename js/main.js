f829c5a906cbe9a10371708f47a0056a8f800c82
Churchinruins <surhoisaev@gmail.com>
2025-02-09 02:13:00 +0300

    change:checking day and text

--- js/main.js
+++ js/main.js
@@ -1,24 +1,21 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', function () {
-
     let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
     let date = new Date();
-
+    let currentDay = date.getDay();
     week.forEach(function (day, index) {
-        let days = document.createElement('div');
-        days.innerHTML = day;
-        document.body.appendChild(days);
-
+        let presentDay = document.createElement('p');
+        presentDay.textContent = day;
+        document.body.appendChild(presentDay);
         if (index === 5 || index === 6) {
-            days.style.fontStyle = 'italic';
+            presentDay.style.fontStyle = 'italic';
         }
-        let currentDay = date.getDay();
         if (currentDay === 0) {
             currentDay = 7;
         }
         if (index === (currentDay - 1)) {
-            days.style.fontWeight = 'bold';
+            presentDay.style.fontWeight = 'bold';
         }
     });
 });
